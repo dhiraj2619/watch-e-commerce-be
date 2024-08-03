b42e1301ae5a89eb58e65faa83063d687663bdff
Dhiraj <dhirajmymodak22@gmail.com>
2024-08-03 18:46:37 +0530

    fixing bugs

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,10 +5,10 @@ const userRouter = require("express").Router();
 userRouter.post("/register", userController.register);
 userRouter.post("/login", userController.login);
 userRouter.post("/logout", userController.logout);
+userRouter.get("/allUsers", userController.getAllUsers);
 userRouter.get("/:id", userController.getUserbyId);
 userRouter.put("/:id", userController.editUserbyId);
 userRouter.delete("/:id", userController.deleteUserbyId);
-userRouter.get("/allUsers", userController.getAllUsers);
 
 userRouter.get("/:userId/cart", userController.getCart);
 userRouter.post("/:userId/cart", userController.addToCart);
